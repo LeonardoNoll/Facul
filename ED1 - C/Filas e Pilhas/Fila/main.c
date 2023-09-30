d5732f4df9e578b9538aa9b6a1836b1b9014ea4e
Leonardo Noll Cruz <leonoll8@hotmail.com>
2023-09-30 00:50:14 +0000

    Filas e Pilhas - Tentando entender

--- ED1 - C/Filas e Pilhas/Fila/main.c
+++ ED1 - C/Filas e Pilhas/Fila/main.c
@@ -1,34 +1,38 @@
     #include<stdio.h>
     #include<stdlib.h>
-    #define INICIO -1
+    #define INICIO 0
     #define FIM 10
 
     int push(int pilha[],int * topo,int valor){
-        if(*topo==FIM){
+        if(*topo-1==FIM){
             return 0;
         } else {
             (*topo)++;
-            pilha[*topo]=valor;
+            pilha[*topo-1]=valor;
             return 1;
         }
     }
 
-    int pop(int pilha[],int * topo,int * valor){
-        if(*topo==INICIO){
+    int pop(int pilha[],int * topo,int * status){
+        int valor;
+        if(*topo==-1){
             return 0;
         } else {
             *valor=pilha[*topo];
             (*topo)--;
-            return 1;
+            return *valor;
         }
     }
 
     int main(){
         int pilha[FIM], topo=INICIO;
         printf("Valor de retorno:%d\n", push(pilha,&topo,10));
-        printf("Valor topo: %d\n",pilha[topo]);
-        push(pilha,&topo,20);
-        printf("Valor topo: %d\n",pilha[topo]);
+        printf("Valor topo: %d\n",pilha[topo-1]);
+        printf("Valor de retorno:%d\n", push(pilha,&topo,20));
+        printf("Valor topo: %d\n",pilha[topo-1]);
+        printf("Valor de retorno:%d\n", push(pilha,&topo,30));
+        printf("Valor topo: %d\n",pilha[topo-1]);
+        printf("Valor de retorno:%d\n", pop(pilha,&topo,40));
         printf("Variável topo:%d\n",topo);
         printf("Array todo\n");
         for(int i=0;i<3;i++){
