a190e01658a1aa2a3ad3e6551a2d763c6c8cc543
Leonardo Noll Cruz <leonoll8@hotmail.com>
2023-10-15 19:27:44 +0000

    Situacao pronto

--- LP2 - Java/NotasAlunos/src/dominio/NotaDisciplina.java
+++ LP2 - Java/NotasAlunos/src/dominio/NotaDisciplina.java
@@ -1,15 +1,14 @@
-package src.dominio;
 //!EM ANDAMENTO!!
+package src.dominio;
 public class NotaDisciplina {
     private float nota;
     private Disciplina disciplina;
     private Situacao situacao;
-    private AnoSemestre anoSemestre; //? N sei se é pra estar aqui
+    private AnoSemestre anoSemestre; //? N sei se é pra estar aqui, acho q deveria
 
     //*Construtor
     protected NotaDisciplina(float n, Disciplina d, Situacao s) {
-        if (d == null) throw new RuntimeException("Disciplina invalida"); //1..1
-        else this.disciplina = d;
+        setDisciplina(d); //1..1
         
         if (n > 10 || n < 0) throw new RuntimeException("Nota invalida");
         else this.nota = n;
@@ -38,12 +37,14 @@ public class NotaDisciplina {
 
 
     //*Setters
-    protected void setAnoSemestre(AnoSemestre anoSemestre) {
-        this.anoSemestre = anoSemestre; //!N sei se precisa de verificação
+    protected void setAnoSemestre(AnoSemestre as) {
+        if (anoSemestre == null) throw new RuntimeException("Ano/semestre invalido");
+        else this.anoSemestre = as; 
     }
 
-    protected void setDisciplina(Disciplina disciplina) {
-        this.disciplina = disciplina; //!N sei se precisa de verificação
+    protected void setDisciplina(Disciplina d) {
+        if (disciplina == null) throw new RuntimeException("Disciplina invalida");
+        else this.disciplina = d; 
     }
     
 }
--- LP2 - Java/NotasAlunos/src/dominio/Situacao.java
+++ LP2 - Java/NotasAlunos/src/dominio/Situacao.java
@@ -1,14 +1,20 @@
+//*ESTADO: PRONTO */
 package src.dominio;
-//!!EM ANDAMENTO!!
 public class Situacao {
     private String situacao;
     // private NotaDisciplina notaDisciplina; //Acho q pode ser retirado
 
     public Situacao(String s){ //TODO questionar o Alex de pq esse método tem nome de construtor mas é void
-        this.situacao = s;
+        setSituacao(s);
     }
 
     public String getSituacao() {
         return situacao;
+    }
+
+    //Não esta explicito mas ache importante ter um setter para a situacao
+    public void setSituacao(String s) {
+        if (s == null) throw new RuntimeException("Situacao invalida");
+        else this.situacao = s;
     }
 }
