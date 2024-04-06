ec24f45747ca9b24e0bb61b3a5a755ec05dae962
Leonardo Noll Cruz <leonoll8@hotmail.com>
2024-04-06 18:04:01 -0300

    consertanto atividade_semana2

--- ED2 - C/Semana 2 - Alocação Dinâmica/atividade_semana2.c
+++ ED2 - C/Semana 2 - Alocação Dinâmica/atividade_semana2.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define tamanho 20
+#define tamanho 300000000
 
 int main() {
-	long int i;
     double *a;
     a = (double *)malloc(tamanho * sizeof(double));
 
-    for(i = 0; i < tamanho; i++) {
+    for(long int i = 0; i < tamanho; i++) {
         a[i] = 0;
     }
+        
+    printf("1. Número de posições de memória ocupada %d",(long int)sizeof(double));
 
+	printf("%x\n", &a[1]);
+	printf("%x\n", &a[2]);
+	printf("%x\n", &a[3]);
+	
     system("pause");
 	free(a);
     return 0;
